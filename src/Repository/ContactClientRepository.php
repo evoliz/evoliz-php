40147d1c55281bde7724619814b92afaa82885c8
dleflour <dylan.leflour25@gmail.com>
2022-03-09 16:06:19 +0100

    Refact BaseRepository in order to have only one entry point for the basic endpoints

--- src/Repository/ContactClientRepository.php
+++ src/Repository/ContactClientRepository.php
@@ -3,9 +3,6 @@
 namespace Evoliz\Client\Repository;
 
 use Evoliz\Client\Config;
-use Evoliz\Client\Exception\ResourceException;
-use Evoliz\Client\Model\ContactClient;
-use Evoliz\Client\Model\Response\ContactClientResponse;
 
 class ContactClientRepository extends BaseRepository
 {
@@ -16,42 +13,5 @@ class ContactClientRepository extends BaseRepository
     public function __construct(Config $config)
     {
         parent::__construct($config);
-    }
-
-    /**
-     * Create a new client contact with given data
-     * @param ContactClient|ContactClientResponse $contactClient
-     * @return ContactClientResponse|string
-     * @throws ResourceException
-     */
-    public function create($contactClient)
-    {
-        if ($contactClient instanceof ContactClientResponse) {
-            $contactClient = new ContactClient((array) $contactClient);
-        }
-
-        $response = $this->config->getClient()->post('api/v1/contacts-clients', [
-            'body' => json_encode($this->mapPayload($contactClient))
-        ]);
-
-        $responseBody = json_decode($response->getBody()->getContents(), true);
-
-        if ($response->getStatusCode() !== 201) {
-            $errorMessage = $responseBody['error'] . ' : ';
-            if ($response->getStatusCode() === 400) {
-                foreach ($responseBody['message'] as $error) {
-                    $errorMessage .= '<br>' . $error[0];
-                }
-            } else {
-                $errorMessage .= $responseBody['message'];
-            }
-            throw new ResourceException($errorMessage, $response->getStatusCode());
-        }
-
-        if ($this->config->getDefaultReturnType() === 'OBJECT') {
-            return new ContactClientResponse($responseBody);
-        } else {
-            return json_encode($responseBody);
-        }
     }
 }
