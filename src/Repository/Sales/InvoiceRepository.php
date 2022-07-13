ff3aedb09ce2db13c69be16754caf2ba6abbcfe9
dleflour <dylan.leflour25@gmail.com>
2022-07-13 10:59:30 +0200

    Trying to run Unit Tests #43

--- src/Repository/Sales/InvoiceRepository.php
+++ src/Repository/Sales/InvoiceRepository.php
@@ -68,11 +68,18 @@ class InvoiceRepository extends BaseRepository
         float $amount,
         \DateTime $paydate = null,
         string $comment = null
-    ) {
-        $requestBody = [
-            'label' => $label,
-            'paytypeid' => $paytypeid,
-            'amount' => $amount,
+    )
+
+
+    {
+        $requestBody =
+            [
+            'label'=>
+                $label,
+            'paytypeid'=>
+                $paytypeid,
+            'amount'=>
+                $amount,
         ];
 
         if ($paydate === null) {
