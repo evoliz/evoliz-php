3069f88e2844a084130f7c777466ed6fc4bbb77c
Brice Fernandez <bfernandez@evoliz.com>
2024-04-03 11:39:34 +0200

    Update SDK to match multi-address feature

---
+++ src/Repository/Clients/ClientDeliveryAddressRepository.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Evoliz\Client\Repository\Clients;
+
+use Evoliz\Client\Config;
+use Evoliz\Client\Repository\BaseRepository;
+use Evoliz\Client\Response\Clients\ClientDeliveryAddressResponse;
+use Evoliz\Client\Response\Clients\ClientResponse;
+
+class ClientDeliveryAddressRepository extends BaseRepository
+{
+    /**
+     * Set up the different parameters for the API requests
+     *
+     * @param  Config $config Configuration for API usage
+     * @throws \Exception
+     */
+    public function __construct(Config $config)
+    {
+        parent::__construct($config, 'client-addresses', ClientDeliveryAddressResponse::class);
+    }
+}
